de27d743c9abd22b19d7f51987104bf9e249c597
AndyZiYe <yeziyu@starrocks.com>
2023-03-09 18:37:40 +0800

    [BugFix] aaa

--- fe/fe-common/src/main/java/com/starrocks/common/jmockit/AutoType.java
+++ fe/fe-common/src/main/java/com/starrocks/common/jmockit/AutoType.java
@@ -10,7 +10,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * 11125443
+ * 111254434
  * Helper class to convert type between Java's wrapper type and primitive type
  * There are 8 wrapper/primitive types in Java:
  * |Wrapped Type         |Primitive Type
