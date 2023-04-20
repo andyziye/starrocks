b02958c27e61db8c95bbed5ef4be39aeb67a18d6
AndyZiYe <yeziyu@starrocks.com>
2023-04-20 15:22:28 +0800

    test

--- fe/fe-core/src/main/java/com/starrocks/alter/Alter.java
+++ fe/fe-core/src/main/java/com/starrocks/alter/Alter.java
@@ -386,7 +386,7 @@ public class Alter {
             GlobalStateMgr.getCurrentState().getEditLog().logAlterMaterializedViewProperties(log);
         }
         LOG.info("alter materialized view properties {}, id: {}", propClone, materializedView.getId());
-        LOG.info("debug !");
+        LOG.info("debug !!");
     }
 
     private void processChangeRefreshScheme(RefreshSchemeDesc refreshSchemeDesc, MaterializedView materializedView,
